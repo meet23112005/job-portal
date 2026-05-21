348c953e8cd8d26473c25f7851cef1de2cc1ac9e
Moradiya Meet <moradiyameet91@gmail.com>
2026-05-21 17:39:39 +0530

    spaces

--- Job_portal.Infrastructure/Services/LocalFileService.cs
+++ Job_portal.Infrastructure/Services/LocalFileService.cs
@@ -69,6 +69,9 @@ namespace Job_portal.Infrastructure.Services
             if (!Directory.Exists(folderPath))
             {
                 Directory.CreateDirectory(folderPath);
+
+
+
             }
             var filePath = Path.Combine(folderPath, uniqueFileName);
 
